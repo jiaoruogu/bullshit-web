42ac3062969178e2f24acb1fd818d93bf242036d
jiaoruogu <ruogu.jiao@outlook.com>
2021-08-27 17:22:46 +0800

    deployed

--- deployed.sh
+++ deployed.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 IMAGE_NAME="bullshit-web-image"
+CONTAINER_NAME="bullshit-web"
+APP_PORT="9010"
 
-#docker ps -a | grep -i ${CON_NAME} > /dev/null 2>&1 && docker stop ${CON_NAME} && docker rm ${CON_NAME}
+docker ps -a | grep -i ${CONTAINER_NAME} > /dev/null 2>&1 && docker stop ${CONTAINER_NAME} && docker rm ${CONTAINER_NAME}
 
-
+docker rmi ${IMAGE_NAME}
 
 docker build -t ${IMAGE_NAME} .
+
+docker run --name ${CONTAINER_NAME} -d -p ${APP_PORT}:80 ${IMAGE_NAME}
