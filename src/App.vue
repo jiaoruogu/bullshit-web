237b54ec65113e490c107603dc868a518948e1c8
jiaoruogu <ruogu.jiao@outlook.com>
2021-08-27 17:30:09 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div style="height:100%" v-if="isLogin">  
       <div class="input-content">
-        <h1>当初的愿望实现了吗？</h1>
+        <h1>当初的愿望实现了吗？？？？？？</h1>
         <div>
           <input v-model="content" type="text">
           <button @click="confirm">确定</button>
